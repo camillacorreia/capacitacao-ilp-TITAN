6553f6dfd059a290d8336b2b975a516aff36c156
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-05-09 14:53:40 -0300

    Exercício 1

---
+++ exercicio-1.py
@@ -0,0 +1,8 @@
+#Crie um algoritmo que leia um número e mostre o seu dobro, seu triplo e sua raiz quadrada.
+n = int(input("Digite um valor: "))
+a=n*2
+b=n*3
+c=n**(1/2)
+print("O dobro de", n,"vale",a)
+print("O triplo de", n,"vale",b)
+print("a raiz quadrada de", n,"vale",c)
